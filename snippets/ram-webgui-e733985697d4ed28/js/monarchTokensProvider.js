b2936a04dbfed5439a7cd531dbdb49a3a19f8e3b
Ddystopia <alexanderbabak@proton.me>
2023-04-17 11:09:22 +0200

    Build

---
+++ snippets/ram-webgui-e733985697d4ed28/js/monarchTokensProvider.js
@@ -0,0 +1,26 @@
+const keywords = [
+  "ADD", "SUB", "MUL", "DIV", "JUMP", "JMP", "JZ", "JZERO", "JGZ",
+  "JGTZ", "LOAD", "STORE", "INPUT", "READ", "WRITE", "OUTPUT", "HALT"
+];
+
+export function makeTokensProvider() {
+  return {
+    keywords: keywords,
+    ignoreCase: true,
+    tokenizer: {
+      root: [
+        [/@?[a-zA-Z][\w$]*/, {
+          cases: {
+            '@keywords': 'keyword',
+            '@default': 'varialbe'
+          }
+        }],
+        [/=\d+/, 'number'],
+        [/\d+/, 'number'],
+        [/[*]/, 'pointer'],
+        [/#.*/, 'comment'],
+        [/:/, 'pointer']
+      ]
+    }
+  }
+};
