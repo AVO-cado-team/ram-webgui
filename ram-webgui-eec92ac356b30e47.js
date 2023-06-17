b2dd27ef6b5c30fd9fc76d21f179006d0fbd5fc7
Oleksandr Babak <alexanderbabak@proton.me>
2023-06-17 21:16:55 +0200

    Upd

--- ram-webgui-eec92ac356b30e47.js
+++ ram-webgui-eec92ac356b30e47.js
@@ -70,7 +70,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('ram-webgui-3a55e86db04eaf7f_bg.wasm', import.meta.url);
+        input = new URL('ram-webgui-eec92ac356b30e47_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
