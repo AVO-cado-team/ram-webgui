ec06e08c37cdaa60d2f41b902c119a506364c367
ic-it <38162697+ic-it@users.noreply.github.com>
2024-02-09 23:28:38 +0000

    Deploying to gh-pages from @ AVO-cado-team/ram-webgui@ad6772ea30a500f539919ed7f049630905f56959 🚀

---
+++ snippets/ram-webgui-962f57e8bf78940b/js/copyToClipboard.js
@@ -0,0 +1,11 @@
+// Copy to the clipboard
+
+export function copyToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    textArea.remove();
+    alert("Copied to clipboard");
+}
